9f5e0c3ae191a0c00187d3946a6032fba45223e1
sachin kumar <sachinshaw6205@gmail.com>
2025-02-15 20:41:47 +0530

    Added codes

---
+++ src/__tests__/App.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import App from '../App';
+
+// eslint-disable--next-line no-undef
+test('renders Vite + React text', () => {
+    render(<App />);
+    const headerElement = screen.getByText(/Vite \+ React/i);
+    except(headerElement).toBeInTheDocument();
+});
+
+test('increments count on button click', () => {
+    render(<App />);
+    const buttonElement = screen.getByText(/count is 0/i);
+    except(buttonElement).toBeInTheDocument();
+
+    fireEvent.click(buttonElement);
+    except(buttonElement).toHaveTextContent('count is 1');
+
+    fireEvent.click(buttonElement);
+    except(buttonElement).toHaveTextContent('count is 2');
+});
